846507f7d60d60af942f061044aec384d413fa13
Francesco Venneri <francesco.venneri@travelperk.com>
2020-07-06 17:52:01 +0200

    [MASTER] Fixed line length

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -34,7 +34,8 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes', 'price', 'link', 'image')
+        fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
+                  'price', 'link', 'image')
         read_only_field = ('id', )
 
 
